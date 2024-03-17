5df7ee54861c137241aecefb34791c1bc68ed5b7
LKSoftware <l.kluijtmans@student.fontys.nl>
2024-03-17 16:50:15 +0100

    Fix: Update version number

--- src/nmbrs/__version__.py
+++ src/nmbrs/__version__.py
@@ -3,7 +3,7 @@ Versioning details.
 """
 
 __title__ = "nmbrs"
-__version__ = "0.0.306"
+__version__ = "0.0.400"
 __author__ = "Lars Kluijtmans"
 __author_email__ = "info@lk-software.com"
 __maintainer__ = "Lars Kluijtmans"
