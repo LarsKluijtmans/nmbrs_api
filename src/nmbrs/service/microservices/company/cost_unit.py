04c99489494776c8cb103a10897cae6b14d8d3ee
Lars kluijtmans <93161087+LarsKluijtmans@users.noreply.github.com>
2024-03-21 17:13:10 +0100

    Fix: Added the cost unit microservice (#75)
    Co-authored-by: LKSoftware <l.kluijtmans@student.fontys.nl>

--- src/nmbrs/service/microservices/company/cost_unit.py
+++ src/nmbrs/service/microservices/company/cost_unit.py
@@ -1,8 +1,10 @@
 """Microservice responsible for cost unit related actions on the company level."""
 
 from zeep import Client
+from zeep.helpers import serialize_object
 
 from ..micro_service import MicroService
+from ....data_classes.company import CostUnit
 from ....utils.nmbrs_exception_handler import nmbrs_exception_handler
 
 
@@ -16,21 +18,39 @@ class CompanyCostUnitService(MicroService):
         self.auth_header = auth_header
 
     @nmbrs_exception_handler(resources=["CompanyService:CostUnit_GetList"])
-    def get(self):
+    def get(self, company_id: int) -> list[CostUnit]:
         """
         Get cost units that belong to a company
 
         For more information, refer to the official documentation:
             [CostUnit_GetList](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=CostUnit_GetList)
+
+        Args:
+            company_id (int): The ID of the company.
+
+        Returns:
+            list[CostUnit]: A list of cost unit objects.
         """
-        raise NotImplementedError()  # pragma: no cover
+        cost_units = self.client.service.CostUnit_GetList(CompanyId=company_id, _soapheaders=self.auth_header)
+        return [CostUnit(cost_unit) for cost_unit in serialize_object(cost_units)]
 
     @nmbrs_exception_handler(resources=["CompanyService:CostUnit_Insert"])
-    def insert(self):
+    def insert(self, company_id: int, cost_unit_id: int, code: str, description: str) -> int:
         """
         Update or insert a Cost Unit into a company.
 
         For more information, refer to the official documentation:
             [CostUnit_Insert](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=CostUnit_Insert)
+
+        Args:
+            company_id (int): The ID of the company.
+            cost_unit_id (int): The ID of the cost unit.
+            code (str): The code of the cost unit.
+            description (str): The description of the cost unit.
+
+        Returns:
+            int: The ID of the inserted or updated cost unit.
         """
-        raise NotImplementedError()  # pragma: no cover
+        cost_unit = {"Id": cost_unit_id, "Code": code, "Description": description}
+        cost_unit_id = self.client.service.CostUnit_Insert(CompanyId=company_id, costunit=cost_unit, _soapheaders=self.auth_header)
+        return cost_unit_id
