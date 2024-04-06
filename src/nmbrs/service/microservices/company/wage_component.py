a584b3e38a4f85cfa39a88f9d5890d6531fac0dc
LKSoftware <l.kluijtmans@student.fontys.nl>
2024-04-06 23:41:49 +0200

    Fix: Added the soap calls

--- src/nmbrs/service/microservices/company/wage_component.py
+++ src/nmbrs/service/microservices/company/wage_component.py
@@ -2,9 +2,12 @@
 """Microservice responsible for wage component related actions on the company level."""
 
 from zeep import Client
+from zeep.helpers import serialize_object
 
 from ..micro_service import MicroService
+from ....data_classes.company import WageComponent
 from ....utils.nmbrs_exception_handler import nmbrs_exception_handler
+from ....utils.return_list import return_list
 
 
 class CompanyWageComponentService(MicroService):
@@ -16,132 +19,271 @@ class CompanyWageComponentService(MicroService):
     def set_auth_header(self, auth_header: dict) -> None:
         self.auth_header = auth_header
 
+    @return_list
     @nmbrs_exception_handler(resources=["CompanyService:WageComponentFixed_Get"])
-    def fixed_get(self):
+    def fixed_get(self, company_id: int, year: int, period: int) -> list[WageComponent]:
         """
         Get all fixed wage components for given period.
 
         For more information, refer to the official documentation:
             [WageComponentFixed_Get](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageComponentFixed_Get)
         """
-        raise NotImplementedError()  # pragma: no cover
+        wage_components = self.client.service.WageComponentFixed_Get(
+            CompanyId=company_id, Period=period, Year=year, _soapheaders=self.auth_header
+        )
+        wage_components = [
+            WageComponent(company_id=company_id, component_type="fixed", data=wage_component)
+            for wage_component in serialize_object(wage_components)
+        ]
+        return wage_components
 
+    @return_list
     @nmbrs_exception_handler(resources=["CompanyService:WageComponentFixed_GetCurrent"])
-    def fixed_get_current(self):
+    def fixed_get_current(self, company_id: int):
         """
         Get all fixed wage components for the current period.
 
         For more information, refer to the official documentation:
             [WageComponentFixed_GetCurrent](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageComponentFixed_GetCurrent)
         """
-        raise NotImplementedError()  # pragma: no cover
+        wage_components = self.client.service.WageComponentFixed_GetCurrent(CompanyId=company_id, _soapheaders=self.auth_header)
+        wage_components = [
+            WageComponent(company_id=company_id, component_type="fixed", data=wage_component)
+            for wage_component in serialize_object(wage_components)
+        ]
+        return wage_components
 
     @nmbrs_exception_handler(resources=["CompanyService:WageComponentFixed_Insert"])
-    def fixed_insert(self):
+    def fixed_insert(
+        self,
+        wage_component: WageComponent,
+        period: int,
+        year: int,
+        protected_mode: bool,
+    ) -> int:
         """
         Insert a wage component to given period. If the period is before the company's current period, unprotected mode flag is required.
 
         For more information, refer to the official documentation:
             [WageComponentFixed_Insert](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageComponentFixed_Insert)
         """
-        raise NotImplementedError()  # pragma: no cover
+        new_wage_component = {"Id": wage_component.id, "Code": wage_component.code, "Value": wage_component.value}
+        response = self.client.service.WageComponentFixed_Insert(
+            CompanyId=wage_component.company_id,
+            WageComponent=new_wage_component,
+            Period=period,
+            Year=year,
+            UnprotectedMode=protected_mode,
+            _soapheaders=self.auth_header,
+        )
+        return response
 
     @nmbrs_exception_handler(resources=["CompanyService:WageComponentFixed_InsertCurrent"])
-    def fixed_insert_current(self):
+    def fixed_insert_current(self, wage_component: WageComponent) -> int:
         """
         Insert a wage component to the current period.
 
         For more information, refer to the official documentation:
             [WageComponentFixed_InsertCurrent](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageComponentFixed_InsertCurrent)
         """
-        raise NotImplementedError()  # pragma: no cover
+        new_wage_component = {"Id": wage_component.id, "Code": wage_component.code, "Value": wage_component.value}
+        response = self.client.service.WageComponentFixed_InsertCurrent(
+            CompanyId=wage_component.company_id, WageComponent=new_wage_component, _soapheaders=self.auth_header
+        )
+        return response
 
+    @return_list
     @nmbrs_exception_handler(resources=["CompanyService:WageComponentFixed_Insert_Batch"])
-    def fixed_insert_batch(self):
+    def fixed_insert_batch(
+        self,
+        wage_components: list[WageComponent],
+        period: int,
+        year: int,
+        protected_mode: bool,
+    ) -> list[int]:
         """
         Insert a batch of wage components to given period. If the period is before the company's current period, unprotected mode flag is required.
 
         For more information, refer to the official documentation:
             [WageComponentFixed_Insert_Batch](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageComponentFixed_Insert_Batch)
         """
-        raise NotImplementedError()  # pragma: no cover
+        new_wage_components = []
+        for wage_component in wage_components:
+            new_wage_components.append(
+                {
+                    "CompanyId": wage_component.company_id,
+                    "Id": wage_component.id,
+                    "Code": wage_component.code,
+                    "Value": wage_component.value,
+                }
+            )
+        response = self.client.service.WageComponentFixed_Insert_Batch(
+            WageComponents=new_wage_components, Period=period, Year=year, UnprotectedMode=protected_mode, _soapheaders=self.auth_header
+        )
+        return response
 
     @nmbrs_exception_handler(resources=["CompanyService:WageComponentFixed_Stop"])
-    def fixed_stop(self):
+    def fixed_stop(
+        self,
+        company_id: int,
+        component_id: int,
+        period: int,
+        year: int,
+        protected_mode: bool,
+    ) -> None:
         """
         Stop a wage component ending after given period. If the period is before the company's current period, unprotected mode flag is required.
 
         For more information, refer to the official documentation:
             [WageComponentFixed_Stop](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageComponentFixed_Stop)
         """
-        raise NotImplementedError()  # pragma: no cover
+        self.client.service.WageComponentFixed_Stop(
+            CompanyId=company_id,
+            WageComponentId=component_id,
+            Period=period,
+            Year=year,
+            UnprotectedMode=protected_mode,
+            _soapheaders=self.auth_header,
+        )
 
+    @return_list
     @nmbrs_exception_handler(resources=["CompanyService:WageComponentVar_Get"])
-    def variable_get(self):
+    def variable_get(self, company_id: int, year: int, period: int) -> list[WageComponent]:
         """
         Get all fixed wage components for given period.
 
         For more information, refer to the official documentation:
             [WageComponentVar_Get](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageComponentVar_Get)
         """
-        raise NotImplementedError()  # pragma: no cover
+        wage_components = self.client.service.WageComponentVar_Get(
+            CompanyId=company_id, Period=period, Year=year, _soapheaders=self.auth_header
+        )
+        wage_components = [
+            WageComponent(company_id=company_id, component_type="variable", data=wage_component)
+            for wage_component in serialize_object(wage_components)
+        ]
+        return wage_components
 
     @nmbrs_exception_handler(resources=["CompanyService:WageComponentVar_GetCurrent"])
-    def variable_get_current(self):
+    def variable_get_current(self, company_id: int) -> list[WageComponent]:
         """
         Get all fixed wage components for the current period.
 
         For more information, refer to the official documentation:
             [WageComponentVar_GetCurrent](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageComponentVar_GetCurrent)
         """
-        raise NotImplementedError()  # pragma: no cover
+        wage_components = self.client.service.WageComponentVar_GetCurrent(CompanyId=company_id, _soapheaders=self.auth_header)
+        wage_components = [
+            WageComponent(company_id=company_id, component_type="variable", data=wage_component)
+            for wage_component in serialize_object(wage_components)
+        ]
+        return wage_components
 
     @nmbrs_exception_handler(resources=["CompanyService:WageComponentVar_Insert"])
-    def variable_insert(self):
+    def variable_insert(
+        self,
+        wage_component: WageComponent,
+        period: int,
+        year: int,
+        protected_mode: bool,
+    ) -> int:
         """
         Insert a wage component to given period. If the period is before the company's current period, unprotected mode flag is required.
 
         For more information, refer to the official documentation:
             [WageComponentVar_Insert](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageComponentVar_Insert)
         """
-        raise NotImplementedError()  # pragma: no cover
+        new_wage_component = {"Id": wage_component.id, "Code": wage_component.code, "Value": wage_component.value}
+        response = self.client.service.WageComponentVar_Insert(
+            CompanyId=wage_component.company_id,
+            WageComponent=new_wage_component,
+            Period=period,
+            Year=year,
+            UnprotectedMode=protected_mode,
+            _soapheaders=self.auth_header,
+        )
+        return response
 
     @nmbrs_exception_handler(resources=["CompanyService:WageComponentVar_InsertCurrent"])
-    def variable_insert_current(self):
+    def variable_insert_current(self, wage_component: WageComponent) -> int:
         """
         Insert a wage component to the current period.
 
         For more information, refer to the official documentation:
             [WageComponentVar_InsertCurrent](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageComponentVar_InsertCurrent)
         """
-        raise NotImplementedError()  # pragma: no cover
+        new_wage_component = {"Id": wage_component.id, "Code": wage_component.code, "Value": wage_component.value}
+        response = self.client.service.WageComponentVar_InsertCurrent(
+            CompanyId=wage_component.company_id, WageComponent=new_wage_component, _soapheaders=self.auth_header
+        )
+        return response
 
+    @return_list
     @nmbrs_exception_handler(resources=["CompanyService:WageComponentVar_Insert_Batch"])
-    def variable_insert_batch(self):
+    def variable_insert_batch(
+        self,
+        wage_components: list[WageComponent],
+        period: int,
+        year: int,
+        protected_mode: bool,
+    ) -> list[int]:
         """
         Insert a batch of wage components to given period. If the period is before the company's current period, unprotected mode flag is required.
 
         For more information, refer to the official documentation:
             [WageComponentVar_Insert_Batch](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageComponentVar_Insert_Batch)
         """
-        raise NotImplementedError()  # pragma: no cover
+        new_wage_components = []
+        for wage_component in wage_components:
+            new_wage_components.append(
+                {
+                    "CompanyId": wage_component.company_id,
+                    "Id": wage_component.id,
+                    "Code": wage_component.code,
+                    "Value": wage_component.value,
+                }
+            )
+        response = self.client.service.WageComponentVar_Insert_Batch(
+            WageComponents=new_wage_components, Period=period, Year=year, UnprotectedMode=protected_mode, _soapheaders=self.auth_header
+        )
+        return response
 
     @nmbrs_exception_handler(resources=["CompanyService:WageComponentVar_Clear"])
-    def variable_clear(self):
+    def variable_clear(
+        self,
+        company_id: int,
+        period: int,
+        year: int,
+        protected_mode: bool,
+    ) -> list[int]:
         """
         Clear all variable wage components for given period.
 
         For more information, refer to the official documentation:
             [WageComponentVar_Clear](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageComponentVar_Clear)
         """
-        raise NotImplementedError()  # pragma: no cover
+        response = self.client.service.WageComponentVar_Clear(
+            CompanyId=company_id,
+            Period=period,
+            Year=year,
+            UnprotectedMode=protected_mode,
+            _soapheaders=self.auth_header,
+        )
+        return response
 
     @nmbrs_exception_handler(resources=["CompanyService:WageComponentVar_ClearCurrent"])
-    def variable_clear_current(self):
+    def variable_clear_current(
+        self,
+        company_id: int,
+    ) -> list[int]:
         """
         Clear all variable wage components for the current period.
 
         For more information, refer to the official documentation:
             [WageComponentVar_ClearCurrent](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageComponentVar_ClearCurrent)
         """
-        raise NotImplementedError()  # pragma: no cover
+        response = self.client.service.WageComponentVar_ClearCurrent(
+            CompanyId=company_id,
+            _soapheaders=self.auth_header,
+        )
+        return response
