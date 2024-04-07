9dba063eaff28fa7043fc821ba041868c356f4f0
LKSoftware <l.kluijtmans@student.fontys.nl>
2024-04-07 12:28:31 +0200

    Fix: Implementation complete, with tests

--- src/nmbrs/service/microservices/company/wage_model.py
+++ src/nmbrs/service/microservices/company/wage_model.py
@@ -1,9 +1,12 @@
 """Microservice responsible for wage model related actions on the company level."""
 
 from zeep import Client
+from zeep.helpers import serialize_object
 
 from ..micro_service import MicroService
+from ....data_classes.company import WageModel
 from ....utils.nmbrs_exception_handler import nmbrs_exception_handler
+from ....utils.return_list import return_list
 
 
 class CompanyWageModelService(MicroService):
@@ -15,22 +18,28 @@ class CompanyWageModelService(MicroService):
     def set_auth_header(self, auth_header: dict) -> None:
         self.auth_header = auth_header
 
+    @return_list
     @nmbrs_exception_handler(resources=["CompanyService:WageModel_GetWageCodes"])
-    def get(self):
+    def get(self, company_id: int) -> list[WageModel]:
         """
         Returns wage codes that belong to a company's wage model.
 
         For more information, refer to the official documentation:
             [WageModel_GetWageCodes](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageModel_GetWageCodes)
         """
-        raise NotImplementedError()  # pragma: no cover
+        wage_models = self.client.service.WageModel_GetWageCodes(CompanyId=company_id, _soapheaders=self.auth_header)
+        wage_models = [WageModel(company_id=company_id, data=wage_model) for wage_model in serialize_object(wage_models)]
+        return wage_models
 
+    @return_list
     @nmbrs_exception_handler(resources=["CompanyService:WageModel2_GetWageCodes"])
-    def get_2(self):
+    def get_2(self, company_id: int) -> list[WageModel]:
         """
         Returns wage codes that belong to a company's wage model.
 
         For more information, refer to the official documentation:
             [WageModel2_GetWageCodes](https://api.nmbrs.nl/soap/v3/CompanyService.asmx?op=WageModel2_GetWageCodes)
         """
-        raise NotImplementedError()  # pragma: no cover
+        wage_models = self.client.service.WageModel2_GetWageCodes(CompanyId=company_id, _soapheaders=self.auth_header)
+        wage_models = [WageModel(company_id=company_id, data=wage_model) for wage_model in serialize_object(wage_models)]
+        return wage_models
