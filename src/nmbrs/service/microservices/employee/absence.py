4b452aa57fd4db7df96a239f6723d015d9062741
LKSoftware <l.kluijtmans@student.fontys.nl>
2024-04-10 10:32:26 +0200

    Fix: employee level absence microservice complete with tests

--- src/nmbrs/service/microservices/employee/absence.py
+++ src/nmbrs/service/microservices/employee/absence.py
@@ -1,10 +1,14 @@
-# pylint: disable=line-too-long
 """Microservice responsible for absence related actions on the employee level."""
 
+from datetime import datetime
+
 from zeep import Client
+from zeep.helpers import serialize_object
 
 from ..micro_service import MicroService
+from ....data_classes.employee import Absence
 from ....utils.nmbrs_exception_handler import nmbrs_exception_handler
+from ....utils.return_list import return_list
 
 
 class EmployeeAbsenceService(MicroService):
@@ -16,82 +20,209 @@ class EmployeeAbsenceService(MicroService):
     def set_auth_header(self, auth_header: dict) -> None:
         self.auth_header = auth_header
 
+    @return_list
     @nmbrs_exception_handler(resource="EmployeeService:Absence_GetList")
-    def get(self):
+    def get(self, employee_id: int) -> list[Absence]:
         """
         Get a list of all absences.
 
         For more information, refer to the official documentation:
             [Absence_GetList](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Absence_GetList)
-        """
-        raise NotImplementedError()  # pragma: no cover
 
+        Args:
+            employee_id (int): The ID of the employee.
+
+        Returns:
+            list[Absence]: A list of Absence objects representing the absences.
+        """
+        absences = self.client.service.Absence_GetList(EmployeeId=employee_id, _soapheaders=self.auth_header)
+        return [Absence(employee_id=employee_id, data=absence) for absence in serialize_object(absences)]
+
+    @return_list
     @nmbrs_exception_handler(resource="EmployeeService:Absence2_GetList")
-    def get_2(self):
+    def get_2(self, employee_id: int) -> list[Absence]:
         """
         Get a list of all absences with their respective cause.
 
         For more information, refer to the official documentation:
             [Absence2_GetList](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Absence2_GetList)
-        """
-        raise NotImplementedError()  # pragma: no cover
 
+        Args:
+            employee_id (int): The ID of the employee.
+
+        Returns:
+            list[Absence]: A list of Absence objects representing the absences.
+        """
+        absences = self.client.service.Absence2_GetList(EmployeeId=employee_id, _soapheaders=self.auth_header)
+        return [Absence(employee_id=employee_id, data=absence) for absence in serialize_object(absences)]
+
+    @return_list
     @nmbrs_exception_handler(resource="EmployeeService:Absence_GetAll_AllEmployeesByCompany")
-    def get_all_by_company(self):
+    def get_all_by_company(self, company_id: int) -> list[Absence]:
         """
         Get a list of all absence of all company employees.
 
         For more information, refer to the official documentation:
-            [Absence_GetAll_AllEmployeesByCompany](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Absence_GetAll_AllEmployeesByCompany)
+            [Absence_GetAll_AllEmployeesByCompany](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Absence_GetAll_AllEmployeesByCompany)  # pylint: disable=line-too-long
+
+        Args:
+            company_id (int): The ID of the company.
+
+        Returns:
+            list[Absence]: A list of Absence objects representing the absences.
         """
-        raise NotImplementedError()  # pragma: no cover
+        absences = self.client.service.Absence_GetAll_AllEmployeesByCompany(CompanyId=company_id, _soapheaders=self.auth_header)
+
+        _absences = []
+        for employee in serialize_object(absences):
+            _absences.append(Absence(employee_id=employee["EmployeeId"], data=employee))
+        return _absences
 
     @nmbrs_exception_handler(resource="EmployeeService:Absence_Insert")
-    def insert(self):
+    def insert(self, employee_id: int, absence: Absence) -> int:
         """
         Insert an absence, this item will start from the given date in the object.
 
         For more information, refer to the official documentation:
             [Absence_Insert](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Absence_Insert)
+
+        Args:
+            employee_id (int): The ID of the employee.
+            absence (Absence): The absence details to insert.
+
+        Returns:
+            int: The response status code indicating the success of the operation.
         """
-        raise NotImplementedError()  # pragma: no cover
+        _absence = {
+            "AbsenceId": absence.id,
+            "Comment": absence.comment,
+            "Percentage": absence.percentage,
+            "Start": absence.start,
+            "RegistrationStartDate": absence.registration_start_date,
+            "End": absence.end,
+            "RegistrationEndDate": absence.registration_end_date,
+            "Dossier": absence.dossier,
+            "Dossiernr": absence.dossier_number,
+        }
+        response = self.client.service.Absence_Insert(EmployeeId=employee_id, Absence=_absence, _soapheaders=self.auth_header)
+        return response
 
     @nmbrs_exception_handler(resource="EmployeeService:Absence2_Insert")
-    def insert_2(self):
+    def insert_2(self, employee_id: int, absence: Absence) -> int:
         """
         Insert an absence with cause, this item will start from the given date in the object.
 
         For more information, refer to the official documentation:
             [Absence2_Insert](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Absence2_Insert)
+
+        Args:
+            employee_id (int): The ID of the employee.
+            absence (Absence): The absence details to insert.
+
+        Returns:
+            int: The response status code indicating the success of the operation.
         """
-        raise NotImplementedError()  # pragma: no cover
+        _absence = {
+            "AbsenceCause": {"CauseId": absence.cause.id, "Cause": absence.cause.cause},
+            "AbsenceId": absence.id,
+            "Comment": absence.comment,
+            "Percentage": absence.percentage,
+            "Start": absence.start,
+            "RegistrationStartDate": absence.registration_start_date,
+            "End": absence.end,
+            "RegistrationEndDate": absence.registration_end_date,
+            "Dossier": absence.dossier,
+            "Dossiernr": absence.dossier_number,
+        }
+        response = self.client.service.Absence2_Insert(EmployeeId=employee_id, Absence=_absence, _soapheaders=self.auth_header)
+        return response
 
     @nmbrs_exception_handler(resource="EmployeeService:Absence_PartialRecoveryInsert")
-    def insert_partial_recovery(self):
+    def insert_partial_recovery(
+        self, employee_id: int, absence_id: int, start_date: datetime, report_data: datetime, percentage: int, comment: str
+    ) -> int:
         """
         Insert an absence partial recovery message.
 
         For more information, refer to the official documentation:
             [Absence_PartialRecoveryInsert](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Absence_PartialRecoveryInsert)
+
+        Args:
+            employee_id (int): The ID of the employee.
+            absence_id (int): The ID of the absence.
+            start_date (datetime): The start date of the absence recovery.
+            report_data (datetime): The report date of the absence recovery.
+            percentage (int): The percentage of the recovery.
+            comment (str): The comment for the recovery.
+
+        Returns:
+            int: The response status code indicating the success of the operation.
         """
-        raise NotImplementedError()  # pragma: no cover
+        response = self.client.service.Absence_PartialRecoveryInsert(
+            EmployeeId=employee_id,
+            AbsenceID=absence_id,
+            StartDate=start_date,
+            Reportdate=report_data,
+            Percent=percentage,
+            Comment=comment,
+            _soapheaders=self.auth_header,
+        )
+        return response
 
     @nmbrs_exception_handler(resource="EmployeeService:Absence_RecoveryInsert")
-    def insert_recovery(self):
+    def insert_recovery(self, employee_id: int, absence_id: int, last_day_absence: datetime, report_data: datetime, comment: str) -> str:
         """
         Insert an absence recovery message.
 
         For more information, refer to the official documentation:
             [Absence_RecoveryInsert](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Absence_RecoveryInsert)
+
+        Args:
+            employee_id (int): The ID of the employee.
+            absence_id (int): The ID of the absence.
+            last_day_absence (datetime): The last day of the absence.
+            report_data (datetime): The report date of the absence recovery.
+            comment (str): The comment for the recovery.
+
+        Returns:
+            str: The response status code indicating the success of the operation.
         """
-        raise NotImplementedError()  # pragma: no cover
+        response = self.client.service.Absence_RecoveryInsert(
+            EmployeeId=employee_id,
+            AbsenceID=absence_id,
+            Lastdayabsence=last_day_absence,
+            Reportdate=report_data,
+            Comment=comment,
+            _soapheaders=self.auth_header,
+        )
+        return response
 
     @nmbrs_exception_handler(resource="EmployeeService:AbsenceNotification_Insert")
-    def insert_notification(self):
+    def insert_notification(self, employee_id: int, absence: Absence) -> int:
         """
         Insert a new absence date, this item will start from the given date in the object to the requested absence dossier.
 
         For more information, refer to the official documentation:
             [AbsenceNotification_Insert](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=AbsenceNotification_Insert)
+
+        Args:
+            employee_id (int): The ID of the employee.
+            absence (Absence): The absence details to insert.
+
+        Returns:
+            int: The response status code indicating the success of the operation.
         """
-        raise NotImplementedError()  # pragma: no cover
+        _absence = {
+            "AbsenceCause": {"CauseId": absence.cause.id, "Cause": absence.cause.cause},
+            "AbsenceId": absence.id,
+            "Comment": absence.comment,
+            "Percentage": absence.percentage,
+            "Start": absence.start,
+            "RegistrationStartDate": absence.registration_start_date,
+            "End": absence.end,
+            "RegistrationEndDate": absence.registration_end_date,
+            "Dossier": absence.dossier,
+            "Dossiernr": absence.dossier_number,
+        }
+        response = self.client.service.AbsenceNotification_Insert(EmployeeId=employee_id, Absence=_absence, _soapheaders=self.auth_header)
+        return response
---
+++ tests/test_nmbrs/test_service/test_microservices/test_employee_microservices/test_absence_microservice.py
@@ -0,0 +1,274 @@
+"""Test cases for the EmployeeAbsenceService class."""
+
+import unittest
+from datetime import datetime
+from unittest.mock import MagicMock
+
+from src.nmbrs.service.microservices.employee.absence import EmployeeAbsenceService, Absence
+
+
+class TestEmployeeAbsenceService(unittest.TestCase):
+    """Test cases for the EmployeeAbsenceService class."""
+
+    def setUp(self):
+        """Set up test environment."""
+        self.client = MagicMock()
+        self.employee_absence_service = EmployeeAbsenceService(self.client)
+        self.mock_auth_header = MagicMock()
+        self.employee_absence_service.set_auth_header(self.mock_auth_header)
+
+    def test_get(self):
+        """Test the get method of EmployeeAbsenceService."""
+        employee_id = 123
+        expected_absences = [
+            {"AbsenceId": 1, "Comment": "Sick leave", "Percentage": 100, "Start": datetime(2023, 1, 1), "End": datetime(2023, 1, 5)},
+            {"AbsenceId": 2, "Comment": "Vacation", "Percentage": 100, "Start": datetime(2023, 2, 1), "End": datetime(2023, 2, 5)},
+        ]
+        self.client.service.Absence_GetList.return_value = expected_absences
+
+        result = self.employee_absence_service.get(employee_id)
+
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), len(expected_absences))
+        for item in result:
+            self.assertIsInstance(item, Absence)
+        self.client.service.Absence_GetList.assert_called_once_with(EmployeeId=employee_id, _soapheaders=self.mock_auth_header)
+
+    def test_get_2(self):
+        """Test the get_2 method of EmployeeAbsenceService."""
+        employee_id = 123
+        expected_absences = [
+            {
+                "AbsenceId": 1,
+                "Comment": "Sick leave",
+                "Percentage": 100,
+                "Start": datetime(2023, 1, 1),
+                "End": datetime(2023, 1, 5),
+                "AbsenceCause": {"CauseId": 1, "Cause": "Sickness"},
+            },
+            {
+                "AbsenceId": 2,
+                "Comment": "Vacation",
+                "Percentage": 100,
+                "Start": datetime(2023, 2, 1),
+                "End": datetime(2023, 2, 5),
+                "AbsenceCause": {"CauseId": 2, "Cause": "Vacation"},
+            },
+        ]
+        self.client.service.Absence2_GetList.return_value = expected_absences
+
+        result = self.employee_absence_service.get_2(employee_id)
+
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), len(expected_absences))
+        for item in result:
+            self.assertIsInstance(item, Absence)
+        self.client.service.Absence2_GetList.assert_called_once_with(EmployeeId=employee_id, _soapheaders=self.mock_auth_header)
+
+    def test_get_all_by_company(self):
+        """Test the get_all_by_company method of EmployeeAbsenceService."""
+        company_id = 456
+        expected_absences = [
+            {
+                "EmployeeId": 123,
+                "AbsenceId": 1,
+                "Comment": "Sick leave",
+                "Percentage": 100,
+                "Start": datetime(2023, 1, 1),
+                "End": datetime(2023, 1, 5),
+            },
+            {
+                "EmployeeId": 456,
+                "AbsenceId": 2,
+                "Comment": "Vacation",
+                "Percentage": 100,
+                "Start": datetime(2023, 2, 1),
+                "End": datetime(2023, 2, 5),
+            },
+        ]
+        self.client.service.Absence_GetAll_AllEmployeesByCompany.return_value = expected_absences
+
+        result = self.employee_absence_service.get_all_by_company(company_id)
+
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), len(expected_absences))
+        for item in result:
+            self.assertIsInstance(item, Absence)
+        self.client.service.Absence_GetAll_AllEmployeesByCompany.assert_called_once_with(
+            CompanyId=company_id, _soapheaders=self.mock_auth_header
+        )
+
+    def test_insert(self):
+        """Test the insert method of EmployeeAbsenceService."""
+        employee_id = 123
+        absence = Absence(
+            employee_id=employee_id,
+            data={
+                "AbsenceId": 1,
+                "Comment": "Sick leave",
+                "Percentage": 100,
+                "Start": datetime(2023, 1, 1),
+                "End": datetime(2023, 1, 5),
+                "RegistrationStartDate": datetime(2023, 1, 1),
+                "RegistrationEndDate": datetime(2023, 1, 5),
+                "Dossier": "Dossier001",
+                "Dossiernr": 123456,
+                "AbsenceCause": {"CauseId": 1, "Cause": "Sickness"},
+            },
+        )
+
+        self.client.service.Absence_Insert.return_value = 200
+
+        response = self.employee_absence_service.insert(employee_id, absence)
+
+        self.assertEqual(response, 200)
+        self.client.service.Absence_Insert.assert_called_once_with(
+            EmployeeId=employee_id,
+            Absence={
+                "AbsenceId": 1,
+                "Comment": "Sick leave",
+                "Percentage": 100,
+                "Start": datetime(2023, 1, 1),
+                "RegistrationStartDate": datetime(2023, 1, 1),
+                "End": datetime(2023, 1, 5),
+                "RegistrationEndDate": datetime(2023, 1, 5),
+                "Dossier": "Dossier001",
+                "Dossiernr": 123456,
+            },
+            _soapheaders=self.mock_auth_header,
+        )
+
+    def test_insert_2(self):
+        """Test the insert_2 method of EmployeeAbsenceService."""
+        employee_id = 123
+        absence = Absence(
+            employee_id=employee_id,
+            data={
+                "AbsenceId": 1,
+                "Comment": "Sick leave",
+                "Percentage": 100,
+                "Start": datetime(2023, 1, 1),
+                "End": datetime(2023, 1, 5),
+                "RegistrationStartDate": datetime(2023, 1, 1),
+                "RegistrationEndDate": datetime(2023, 1, 5),
+                "Dossier": "Dossier001",
+                "Dossiernr": 123456,
+                "AbsenceCause": {"CauseId": 1, "Cause": "Sickness"},
+            },
+        )
+
+        self.client.service.Absence2_Insert.return_value = 200
+
+        response = self.employee_absence_service.insert_2(employee_id, absence)
+
+        self.assertEqual(response, 200)
+        self.client.service.Absence2_Insert.assert_called_once_with(
+            EmployeeId=employee_id,
+            Absence={
+                "AbsenceCause": {"CauseId": 1, "Cause": "Sickness"},
+                "AbsenceId": 1,
+                "Comment": "Sick leave",
+                "Percentage": 100,
+                "Start": datetime(2023, 1, 1),
+                "RegistrationStartDate": datetime(2023, 1, 1),
+                "End": datetime(2023, 1, 5),
+                "RegistrationEndDate": datetime(2023, 1, 5),
+                "Dossier": "Dossier001",
+                "Dossiernr": 123456,
+            },
+            _soapheaders=self.mock_auth_header,
+        )
+
+    def test_insert_partial_recovery(self):
+        """Test the insert_partial_recovery method of EmployeeAbsenceService."""
+        employee_id = 123
+        response_status_code = 200
+        absence_id = 1
+        start_date = datetime(2023, 1, 1)
+        report_date = datetime(2023, 1, 5)
+        percentage = 50
+        comment = "Partial recovery"
+
+        self.client.service.Absence_PartialRecoveryInsert.return_value = response_status_code
+
+        response = self.employee_absence_service.insert_partial_recovery(
+            employee_id, absence_id, start_date, report_date, percentage, comment
+        )
+
+        self.assertEqual(response, response_status_code)
+        self.client.service.Absence_PartialRecoveryInsert.assert_called_once_with(
+            EmployeeId=employee_id,
+            AbsenceID=absence_id,
+            StartDate=start_date,
+            Reportdate=report_date,
+            Percent=percentage,
+            Comment=comment,
+            _soapheaders=self.mock_auth_header,
+        )
+
+    def test_insert_recovery(self):
+        """Test the insert_recovery method of EmployeeAbsenceService."""
+        employee_id = 123
+        response_status_code = "200"
+        absence_id = 1
+        last_day_absence = datetime(2023, 1, 5)
+        report_date = datetime(2023, 1, 10)
+        comment = "Recovery"
+
+        self.client.service.Absence_RecoveryInsert.return_value = response_status_code
+
+        response = self.employee_absence_service.insert_recovery(employee_id, absence_id, last_day_absence, report_date, comment)
+
+        self.assertEqual(response, response_status_code)
+        self.client.service.Absence_RecoveryInsert.assert_called_once_with(
+            EmployeeId=employee_id,
+            AbsenceID=absence_id,
+            Lastdayabsence=last_day_absence,
+            Reportdate=report_date,
+            Comment=comment,
+            _soapheaders=self.mock_auth_header,
+        )
+
+    def test_insert_notification(self):
+        """Test the insert_notification  method of EmployeeAbsenceService."""
+        employee_id = 123
+        absence = Absence(
+            employee_id=employee_id,
+            data={
+                "AbsenceId": 1,
+                "Comment": "Sick leave",
+                "Percentage": 100,
+                "Start": datetime(2023, 1, 1),
+                "End": datetime(2023, 1, 5),
+                "RegistrationStartDate": datetime(2023, 1, 1),
+                "RegistrationEndDate": datetime(2023, 1, 5),
+                "Dossier": "Dossier001",
+                "Dossiernr": 123456,
+                "AbsenceCause": {"CauseId": 1, "Cause": "Sickness"},
+            },
+        )
+
+        self.client.service.AbsenceNotification_Insert.return_value = 200
+
+        response = self.employee_absence_service.insert_notification(employee_id, absence)
+
+        self.assertEqual(response, 200)
+        self.client.service.AbsenceNotification_Insert.assert_called_once_with(
+            EmployeeId=employee_id,
+            Absence={
+                "AbsenceCause": {"CauseId": 1, "Cause": "Sickness"},
+                "AbsenceId": 1,
+                "Comment": "Sick leave",
+                "Percentage": 100,
+                "Start": datetime(2023, 1, 1),
+                "RegistrationStartDate": datetime(2023, 1, 1),
+                "End": datetime(2023, 1, 5),
+                "RegistrationEndDate": datetime(2023, 1, 5),
+                "Dossier": "Dossier001",
+                "Dossiernr": 123456,
+            },
+            _soapheaders=self.mock_auth_header,
+        )
