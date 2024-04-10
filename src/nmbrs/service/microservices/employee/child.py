7e289a94896309b21b8b5a2bff7045686df94ace
LKSoftware <l.kluijtmans@student.fontys.nl>
2024-04-10 13:45:02 +0200

    Fix: Added child microservice with tests

--- src/nmbrs/service/microservices/employee/child.py
+++ src/nmbrs/service/microservices/employee/child.py
@@ -1,9 +1,12 @@
 """Microservice responsible for child related actions on the employee level."""
 
 from zeep import Client
+from zeep.helpers import serialize_object
 
 from ..micro_service import MicroService
+from ....data_classes.employee import Child
 from ....utils.nmbrs_exception_handler import nmbrs_exception_handler
+from ....utils.return_list import return_list
 
 
 class EmployeeChildService(MicroService):
@@ -15,62 +18,160 @@ class EmployeeChildService(MicroService):
     def set_auth_header(self, auth_header: dict) -> None:
         self.auth_header = auth_header
 
+    @return_list
     @nmbrs_exception_handler(resource="EmployeeService:Children_Get")
-    def get(self):
+    def get(self, employee_id: int) -> list[Child]:
         """
         Get employee childs.
 
         For more information, refer to the official documentation:
             [Children_Get](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Children_Get)
-        """
-        raise NotImplementedError()  # pragma: no cover
 
+        Args:
+            employee_id (int): The ID of the employee.
+
+        Returns:
+            list[Child]: A list of Child objects representing the children.
+        """
+        children = self.client.service.Children_Get(EmployeeId=employee_id, _soapheaders=self.auth_header)
+        children = serialize_object(children)
+
+        _children = []
+        for child in children["EmployeeChildren"]["Child"]:
+            _children.append(Child(employee_id=children["EmployeeId"], data=child))
+        return _children
+
+    @return_list
     @nmbrs_exception_handler(resource="EmployeeService:Children_GetAll_Employeesbycompany")
-    def get_all_by_company(self):
+    def get_all_by_company(self, company_id: int) -> list[Child]:
         """
         Get employee childs.
 
         For more information, refer to the official documentation:
             [Children_GetAll_Employeesbycompany](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Children_GetAll_Employeesbycompany)
+
+        Args:
+            company_id (int): The ID of the company.
+
+        Returns:
+            list[Child]: A list of Child objects representing the children.
         """
-        raise NotImplementedError()  # pragma: no cover
+        children = self.client.service.Children_GetAll_Employeesbycompany(CompanyId=company_id, _soapheaders=self.auth_header)
+
+        _children = []
+        for employee in serialize_object(children):
+            for child in employee["EmployeeChildren"]["Child"]:
+                _children.append(Child(employee_id=employee["EmployeeId"], data=child))
+        return _children
 
     @nmbrs_exception_handler(resource="EmployeeService:Child_Delete")
-    def delete(self):
+    def delete(self, employee_id: int, child_id: int) -> str:
         """
-        Delete's child.
+        Deletes child.
 
         For more information, refer to the official documentation:
             [Child_Delete](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Child_Delete)
+
+        Args:
+            employee_id (int): The ID of the employee.
+            child_id (int): The ID of the child.
+
+        Returns:
+            str: The response indicating the success of the operation.
         """
-        raise NotImplementedError()  # pragma: no cover
+        response = self.client.service.Child_Delete(EmployeeId=employee_id, ChildId=child_id, _soapheaders=self.auth_header)
+        return response
 
     @nmbrs_exception_handler(resource="EmployeeService:Children_Insert")
-    def insert(self):
+    def insert(self, employee_id: int, child: Child) -> str:
         """
         Insert an employee child.
 
         For more information, refer to the official documentation:
             [Children_Insert](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Children_Insert)
+
+        Args:
+            employee_id (int): The ID of the employee.
+            child (Child): The Child object to insert.
+
+        Returns:
+            str: A str indicating the success of the operation.
         """
-        raise NotImplementedError()  # pragma: no cover
+        _child = {
+            "Id": child.id,
+            "Name": child.name,
+            "FirstName": child.first_name,
+            "Initials": child.initials,
+            "Gender": child.gender,
+            "Birthday": child.birthday,
+        }
+        response = self.client.service.Children_Insert(
+            EmployeeId=employee_id,
+            child=_child,
+            _soapheaders=self.auth_header,
+        )
+        return response
 
     @nmbrs_exception_handler(resource="EmployeeService:Children_InsertBatch")
-    def insert_batch(self):
+    def insert_batch(self, employee_id: int, children: list[Child]) -> str:
         """
         Insert employee children.
 
         For more information, refer to the official documentation:
             [Children_InsertBatch](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Children_InsertBatch)
+
+        Args:
+            employee_id (int): The ID of the employee.
+            children (list[Child]): The list of Child objects to insert.
+
+        Returns:
+            str: A str indicating the success of the operation.
         """
-        raise NotImplementedError()  # pragma: no cover
+        _children = []
+        for child in children:
+            _children.append(
+                {
+                    "Id": child.id,
+                    "Name": child.name,
+                    "FirstName": child.first_name,
+                    "Initials": child.initials,
+                    "Gender": child.gender,
+                    "Birthday": child.birthday,
+                }
+            )
+        response = self.client.service.Children_InsertBatch(
+            EmployeeId=employee_id,
+            children=_children,
+            _soapheaders=self.auth_header,
+        )
+        return response
 
     @nmbrs_exception_handler(resource="EmployeeService:Children_Update")
-    def update(self):
+    def update(self, employee_id: int, child: Child) -> str:
         """
         Update employee child.
 
         For more information, refer to the official documentation:
             [Children_Update](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Children_Update)
+
+        Args:
+            employee_id (int): The ID of the employee.
+            child (Child): The Child object to insert.
+
+        Returns:
+            str: A str indicating the success of the operation.
         """
-        raise NotImplementedError()  # pragma: no cover
+        _child = {
+            "Id": child.id,
+            "Name": child.name,
+            "FirstName": child.first_name,
+            "Initials": child.initials,
+            "Gender": child.gender,
+            "Birthday": child.birthday,
+        }
+        response = self.client.service.Children_Update(
+            EmployeeId=employee_id,
+            child=_child,
+            _soapheaders=self.auth_header,
+        )
+        return response
